aef2b933b0c01126ef893f2d39013acf741c21d9
Alexander Orlov <me@0xceed.com>
2021-06-15 02:03:42 +0300

    Rename example.

--- examples/undat.rs
+++ examples/undat.rs
@@ -2,13 +2,13 @@ use clap::Clap;
 use std::fs::File;
 
 #[derive(Clap)]
-#[clap(version = "0.1.0", author = "Alexander O. <me@0xceed.com>")]
+#[clap()]
 struct Options {
     file: String
 }
 
 fn main() {
-	let options = Options::parse();
+    let options = Options::parse();
     let file = File::open(options.file).unwrap();
     
     let dirs_count = libformats::dat::count_dirs(&file).unwrap();
