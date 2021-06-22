8a6bbf71b22451a32e9e4829872eafd528ec9144
Alexander Orlov <me@0xceed.com>
2021-06-23 00:13:16 +0300

    Refactor commands.

--- examples/undat.rs
+++ examples/undat.rs
@@ -2,24 +2,53 @@ use clap::Clap;
 use std::fs::File;
 
 #[derive(Clap)]
-#[clap()]
 struct Options {
+    #[clap(subcommand)]
+    subcommand: Subcommand,
     file: String
+}
+
+#[derive(Clap)]
+enum Subcommand {
+    Extract, List(List), Count(Count)
+}
+
+#[derive(Clap)]
+enum List {
+    Files, Directories
+}
+
+#[derive(Clap)]
+enum Count {
+    Files, Directories
 }
 
 fn main() {
     let options = Options::parse();
     let file = File::open(options.file).unwrap();
-    
+
     let dirs_count = libformats::dat::count_dirs(&file).unwrap();
-    println!("Directories: {:?}.", dirs_count);
+    let dirs = libformats::dat::list_dirs(&file, &dirs_count).unwrap();
+    let files = libformats::dat::list_files(&file, &dirs).unwrap();
 
-    let dirs = libformats::dat::list_dirs(&file, dirs_count).unwrap();
-    println!("Listing: {:?}.", dirs.names);
-
-    let files = libformats::dat::list_files(&file, dirs).unwrap();
-    println!("Files: {:?}.", files.len());
-
-    let paths = files.iter().map(|f| f.path.to_owned()).collect::<Vec<String>>();
-    println!("Listing: {:?}.", paths);
+    match options.subcommand {
+        Subcommand::Extract => { unimplemented!(); },
+        Subcommand::List(subject) => {
+            match subject {
+                List::Directories => { println!("{:?}", &dirs.names); },
+                List::Files => { 
+                    let paths = &files.iter().map(|f| f.path.to_owned()).collect::<Vec<String>>();
+                    println!("{:?}", paths); 
+                }
+            }
+        },
+        Subcommand::Count(subject) => {
+            match subject {
+                Count::Directories => { println!("{:?}", &dirs_count); },
+                Count::Files => { 
+                    println!("{:?}", &files.len()); 
+                }
+            }
+        }
+    }
 }
