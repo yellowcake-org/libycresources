567ce7794219b36b80697108adf77867bcb6feec
Alexander Orlov <me@0xceed.com>
2021-06-28 23:53:14 +0300

    Better CLI interface.

--- examples/undat.rs
+++ examples/undat.rs
@@ -7,30 +7,10 @@ use std::io::Write;
 #[clap(version, author)]
 struct Options {
     input: String,
-    #[clap(subcommand)]
-    subcommand: Subcommand,
-}
-
-#[derive(Clap)]
-enum Subcommand {
-    /// Lists all directories contained within provided file.
-    Dirs,
-    /// Lists all files, including full path. Filtered, if directory specified.
-    Files(Files),
-    /// Extracts all contents to destination directory.
-    Unpack(Unpack),
-}
-
-#[derive(Clap)]
-struct Unpack {
-    /// Destination directory.
-    output: String,
-}
-
-#[derive(Clap)]
-struct Files {
-    /// If presented, list files within this directory only.
-    directory: Option<String>,
+    #[clap(short, long)]
+    list: bool,
+    #[clap(short, long)]
+    extract: Option<String>,
 }
 
 fn main() {
@@ -58,72 +38,60 @@ fn main() {
         Ok(value) => value,
     };
 
-    match options.subcommand {
-        Subcommand::Dirs => {
-            for (idx, dir) in dirs.names.iter().enumerate() {
-                println!("[{:?}]: {:?}", idx, dir);
-            }
+    if options.list {
+        for file in &files {
+            println!("{:?}", file.path);
         }
-        Subcommand::Files(options) => {
-            for (idx, file) in files.iter().enumerate() {
-                if let Some(name) = &options.directory {
-                    if !file.path.starts_with(name) {
-                        continue;
-                    }
-                }
+    }
 
-                println!("[{:?}]: {:?}", idx, file.path);
-            }
-        }
-        Subcommand::Unpack(cmd) => {
-            for header in files {
-                println!("Extracting {:?}...", &header.path);
+    if let Some(output) = options.extract {
+        for header in &files {
+            println!("Extracting {:?}...", &header.path);
 
-                let mut extracted = match libformats::dat::bytes(&file, &header) {
-                    Ok(value) => value,
-                    Err(error) => {
-                        eprintln!("Extraction error: {:?}.", error);
-                        continue;
-                    }
-                };
-
-                let root = std::path::Path::new(&cmd.output);
-                let joined = root.join(header.path);
-                let path = joined.as_path();
-
-                let directory = match path.parent() {
-                    None => {
-                        eprintln!("Parent for path \"{:?}\" is not valid!", path);
-                        continue;
-                    }
-                    Some(directory) => directory,
-                };
-
-                if let Err(error) = std::fs::create_dir_all(&directory) {
-                    eprintln!("Directory creation error: {:?}.", error);
+            let mut extracted = match libformats::dat::bytes(&file, &header) {
+                Ok(value) => value,
+                Err(error) => {
+                    eprintln!("Extraction error: {:?}.", error);
                     continue;
                 }
+            };
 
-                let mut created = match std::fs::File::create(&path) {
-                    Err(error) => {
-                        eprintln!("File creation error: {:?}.", error);
-                        continue;
-                    }
-                    Ok(created) => created,
-                };
+            let root = std::path::Path::new(&output);
+            let joined = root.join(&header.path);
+            let path = joined.as_path();
 
-                let written = match created.write(&mut extracted) {
-                    Err(error) => {
-                        eprintln!("File writing error: {:?}.", error);
-                        continue;
-                    }
-                    Ok(value) => value,
-                } as u32;
-
-                if extracted.len() != written as usize {
-                    eprintln!("Attention! Written bytes aren't equal to extracted.");
-                    eprintln!("Hence the file is corrupted.")
+            let directory = match path.parent() {
+                None => {
+                    eprintln!("Parent for path \"{:?}\" is not valid!", path);
+                    continue;
                 }
+                Some(directory) => directory,
+            };
+
+            if let Err(error) = std::fs::create_dir_all(&directory) {
+                eprintln!("Directory creation error: {:?}.", error);
+                continue;
+            }
+
+            let mut created = match std::fs::File::create(&path) {
+                Err(error) => {
+                    eprintln!("File creation error: {:?}.", error);
+                    continue;
+                }
+                Ok(created) => created,
+            };
+
+            let written = match created.write(&mut extracted) {
+                Err(error) => {
+                    eprintln!("File writing error: {:?}.", error);
+                    continue;
+                }
+                Ok(value) => value,
+            } as u32;
+
+            if extracted.len() != written as usize {
+                eprintln!("Attention! Written bytes aren't equal to extracted.");
+                eprintln!("Hence the file is corrupted.")
             }
         }
     }
