00eac91ff823c7c646bd94dd0c41f32f4a72757d
Alexander Orlov <me@0xceed.com>
2022-04-30 23:32:56 +0400

    Fix common .pro flags parsing.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- src/formats/pro.rs
+++ src/formats/pro.rs
@@ -39,6 +39,7 @@ pub mod meta {
                 Steam,
                 Energy,
                 Red,
+                End,
             }
 
             #[derive(PartialEq, Eq, Hash)]
--- src/formats/pro/parse/flags.rs
+++ src/formats/pro/parse/flags.rs
@@ -11,60 +11,62 @@ pub(crate) fn instance<S: Read>(source: &mut S) -> Result<HashSet<meta::info::fl
 
     let mut flags: HashSet<meta::info::flags::Instance> = HashSet::new();
 
-    if (flags_bytes[0] & 0x08) == 0x08 {
+    if (flags_bytes[3] & 0x08) == 0x08 {
         if !flags.insert(meta::info::flags::Instance::Flat) {
             return Err(errors::Error::Format(errors::Format::Flags));
         }
     }
 
-    if (flags_bytes[0] & 0x10) == 0x10 {
+    if (flags_bytes[3] & 0x10) == 0x10 {
         if !flags.insert(meta::info::flags::Instance::NotBlocking) {
             return Err(errors::Error::Format(errors::Format::Flags));
         }
     }
 
-    if (flags_bytes[1] & 0x08) == 0x08 {
+    if (flags_bytes[2] & 0x08) == 0x08 {
         if !flags.insert(meta::info::flags::Instance::MultiHex) {
             return Err(errors::Error::Format(errors::Format::Flags));
         }
     }
 
-    if (flags_bytes[1] & 0x10) == 0x10 {
+    if (flags_bytes[2] & 0x10) == 0x10 {
         if !flags.insert(meta::info::flags::Instance::NotBordered) {
             return Err(errors::Error::Format(errors::Format::Flags));
         }
     }
 
-    if (flags_bytes[3] & 0x20) == 0x20 {
-        if !flags.insert(meta::info::flags::Instance::LightThrough) {
-            return Err(errors::Error::Format(errors::Format::Flags));
-        }
-    }
-
-    if (flags_bytes[3] & 0x80) == 0x80 {
-        if !flags.insert(meta::info::flags::Instance::ShotThrough) {
-            return Err(errors::Error::Format(errors::Format::Flags));
-        }
-    }
-
     if !flags.insert(meta::info::flags::Instance::Transparency(
-        if (flags_bytes[1] & 0x80) == 0x80 {
+        if (flags_bytes[2] & 0x80) == 0x80 {
             None
-        } else if (flags_bytes[1] & 0x40) == 0x40 {
+        } else if (flags_bytes[2] & 0x40) == 0x40 {
             Some(meta::info::flags::Transparency::Red)
-        } else if (flags_bytes[2] & 0x01) == 0x01 {
+        } else if (flags_bytes[1] & 0x01) == 0x01 {
             Some(meta::info::flags::Transparency::Wall)
-        } else if (flags_bytes[2] & 0x02) == 0x02 {
+        } else if (flags_bytes[1] & 0x02) == 0x02 {
             Some(meta::info::flags::Transparency::Glass)
-        } else if (flags_bytes[2] & 0x04) == 0x04 {
+        } else if (flags_bytes[1] & 0x04) == 0x04 {
             Some(meta::info::flags::Transparency::Steam)
-        } else if (flags_bytes[2] & 0x08) == 0x08 {
+        } else if (flags_bytes[1] & 0x08) == 0x08 {
             Some(meta::info::flags::Transparency::Energy)
+        } else if (flags_bytes[0] & 0x10) == 0x10 {
+            Some(meta::info::flags::Transparency::End)
         } else {
             return Err(errors::Error::Format(errors::Format::Flags));
         }
     )) {
         return Err(errors::Error::Format(errors::Format::Flags));
+    }
+
+    if (flags_bytes[0] & 0x20) == 0x20 {
+        if !flags.insert(meta::info::flags::Instance::LightThrough) {
+            return Err(errors::Error::Format(errors::Format::Flags));
+        }
+    }
+
+    if (flags_bytes[0] & 0x80) == 0x80 {
+        if !flags.insert(meta::info::flags::Instance::ShotThrough) {
+            return Err(errors::Error::Format(errors::Format::Flags));
+        }
     }
 
     Ok(flags)
