2e3b03cc6c44f15fad845f61296a39e1d89665b2
Alexander Orlov <me@0xceed.com>
2022-04-30 17:17:43 +0400

    Parsing ladders (bottom and top).
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- src/formats/pro/parse/type/scenery/type/ladder.rs
+++ src/formats/pro/parse/type/scenery/type/ladder.rs
@@ -1,6 +1,18 @@
 use super::super::super::*;
 
-pub(crate) fn instance<S: Read>(source: &mut S, directions: object::scenery::ladder::Direction)
+pub(crate) fn instance<S: Read>(source: &mut S, direction: object::scenery::ladder::Direction)
                                 -> Result<object::scenery::ladder::Instance, errors::Error> {
-    todo!("ladder")
+    let mut destination_bytes = [0u8; 4];
+    match source.read_exact(&mut destination_bytes) {
+        Err(error) => return Err(errors::Error::Read(error)),
+        Ok(value) => value,
+    };
+
+    Ok(object::scenery::ladder::Instance {
+        direction,
+        destination: match object::common::map::Destination::try_from(destination_bytes) {
+            Ok(value) => value,
+            Err(error) => return Err(error),
+        },
+    })
 }
