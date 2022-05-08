6011bce6244294bd3185060baca1792baf52da2d
Alexander Orlov <me@0xceed.com>
2022-05-08 20:54:36 +0400

    Reformat.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- examples/mapview/print.rs
+++ examples/mapview/print.rs
@@ -5,15 +5,17 @@ pub fn map(map: &map::Map) {
     println!("Version: {:?}", map.version);
     println!("Name: {:?}", map.filename);
     println!();
+
     println!("Flags: {:?}", map.flags);
     println!("Elevations: {:?}", map.elevations);
     println!();
+
     println!("Ticks: {:?}", map.ticks);
     println!("Darkness: {:?}", map.darkness);
     println!();
+
     println!("Player's Defaults");
     println!();
-
     println!("Position: {:?}", map.defaults.position);
     println!("Elevation: {:?}", map.defaults.elevation);
     println!("Orientation: {:?}", map.defaults.orientation);
