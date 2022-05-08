3a9d4a686537ee186949b90ec9e1142cbeb86fbb
Alexander Orlov <me@0xceed.com>
2022-05-08 23:44:33 +0400

    Printing tiles.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- examples/mapview/print.rs
+++ examples/mapview/print.rs
@@ -1,5 +1,7 @@
 use libycresources::formats::map;
 
+mod tiles;
+
 pub fn map(map: &map::Map) {
     println!("Map ID: {:?}", map.id);
     println!("Version: {:?}", map.version);
@@ -25,42 +27,5 @@ pub fn map(map: &map::Map) {
     println!("Global: {:?}", map.variables.global);
     println!();
 
-    println!("Tiles");
-    println!();
-
-    for (idx, elevation) in map.tiles.iter().enumerate() {
-        match elevation {
-            None => println!("Level {:?} has not presented.", idx),
-            Some(e) => {
-                println!("Level {:?}", idx);
-                println!();
-
-                println!("Floor");
-                for h in e.floor {
-                    for v in h {
-                        match v {
-                            None => print!("."),
-                            Some(_) => print!("0"),
-                        }
-                    }
-
-                    println!();
-                }
-                println!();
-
-                println!("Roof");
-                for h in e.roof {
-                    for v in h {
-                        match v {
-                            None => print!("."),
-                            Some(_) => print!("0"),
-                        }
-                    }
-
-                    println!();
-                }
-                println!();
-            }
-        }
-    }
+    self::tiles::print(map);
 }
---
+++ examples/mapview/print/tiles.rs
@@ -0,0 +1,42 @@
+use libycresources::formats::map;
+
+pub(crate) fn print(map: &map::Map) {
+    println!("Tiles");
+    println!();
+
+    for (idx, elevation) in map.tiles.iter().enumerate() {
+        match elevation {
+            None => println!("Level {:?} has not presented.", idx),
+            Some(e) => {
+                println!("Level {:?}", idx);
+                println!();
+
+                println!("Floor");
+                for h in e.floor {
+                    for v in h {
+                        match v {
+                            None => print!("."),
+                            Some(_) => print!("0"),
+                        }
+                    }
+
+                    println!();
+                }
+                println!();
+
+                println!("Roof");
+                for h in e.roof {
+                    for v in h {
+                        match v {
+                            None => print!("."),
+                            Some(_) => print!("0"),
+                        }
+                    }
+
+                    println!();
+                }
+                println!();
+            }
+        }
+    }
+}
