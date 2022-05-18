01b1e218e938cd1ce65e48e22234e8674db1af17
Alexander Orlov <me@0xceed.com>
2022-05-19 01:37:44 +0400

    Parsing Elevator patch.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- src/formats/map/parse/prototypes/prototype/patch/scenery.rs
+++ src/formats/map/parse/prototypes/prototype/patch/scenery.rs
@@ -12,7 +12,7 @@ pub(crate) fn patch<S: Read>(source: &mut S, scenery: &Instance, read_ladders_ma
     Ok(match &scenery.body {
         Body::Door(_) => { Patch::Door(door::patch(source)?) }
         Body::Stairs(_) => { Patch::Stairs(stairs::patch(source)?) }
-        Body::Elevator(elevator) => { Patch::Elevator(elevator::patch(source, elevator)?) }
+        Body::Elevator(_) => { Patch::Elevator(elevator::patch(source)?) }
         Body::Ladder(_) => { Patch::Ladder(ladder::patch(source, read_ladders_map)?) }
         Body::Generic(_) => { Patch::Generic(()) }
     })
--- src/formats/map/parse/prototypes/prototype/patch/scenery/elevator.rs
+++ src/formats/map/parse/prototypes/prototype/patch/scenery/elevator.rs
@@ -1,8 +1,16 @@
 use std::io::Read;
 
-use crate::common::types::errors;
-use crate::formats::pro::object::scenery::elevator;
+use byteorder::{BigEndian, ReadBytesExt};
 
-pub(crate) fn patch<S: Read>(source: &mut S, elevator: &elevator::Instance) -> Result<elevator::Patch, errors::Error> {
-    todo!()
+use crate::common::types::errors;
+use crate::formats::pro::object::scenery::elevator::Patch;
+use crate::formats::pro::traits::TryFromOptional;
+
+pub(crate) fn patch<S: Read>(source: &mut S) -> Result<Patch, errors::Error> {
+    let r#type = u16::try_from_optional(source.read_i32::<BigEndian>()?, -1)
+        .map_err(|_| errors::Error::Format)?;
+
+    let elevation = source.read_i32::<BigEndian>()?;
+
+    Ok(Patch { floor: elevation, r#type })
 }
