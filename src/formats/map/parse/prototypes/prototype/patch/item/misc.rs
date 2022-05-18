f4d767c089e6c014957b8a513363787f08df8bfd
Alexander Orlov <me@0xceed.com>
2022-05-18 20:19:05 +0400

    Parsing Weapons patch.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- src/formats/map/parse/prototypes/prototype/patch/item/misc.rs
+++ src/formats/map/parse/prototypes/prototype/patch/item/misc.rs
@@ -3,7 +3,5 @@ use crate::formats::pro::object::item::misc::Patch;
 use super::super::super::*;
 
 pub fn patch<S: Read>(source: &mut S) -> Result<Patch, errors::Error> {
-    Ok(Patch {
-        count: u32::try_from(source.read_i32::<BigEndian>()?).ok()
-    })
+    Ok(Patch { count: u32::try_from(source.read_i32::<BigEndian>()?).ok() })
 }
--- src/formats/map/parse/prototypes/prototype/patch/item/weapon.rs
+++ src/formats/map/parse/prototypes/prototype/patch/item/weapon.rs
@@ -3,5 +3,8 @@ use crate::formats::pro::object::item::weapon::Patch;
 use super::super::super::*;
 
 pub fn patch<S: Read>(source: &mut S) -> Result<Patch, errors::Error> {
-    todo!()
+    let rounds = source.read_u32::<BigEndian>()?;
+    let ammo_item_id = u16::try_from(source.read_i32::<BigEndian>()?).ok();
+
+    Ok(Patch { rounds, ammo_item_id })
 }
