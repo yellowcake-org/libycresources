6e73f2ccb4c9c1fb8e2160b4f5cb6112267347e2
Alexander Orlov <me@0xceed.com>
2022-12-25 01:29:24 +0400

    CORE-41: Removed indexing lists unsafely.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- examples/fontview/print.rs
+++ examples/fontview/print.rs
@@ -4,18 +4,23 @@ pub fn glyph(glyph: &aaf::Glyph) {
     if !glyph.dots.is_empty() {
         for row in 0..glyph.height {
             for column in 0..glyph.width {
-                let pixel = &glyph.dots[(row * glyph.width + column) as usize];
-                let levels = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'];
+                let index = (row * glyph.width + column) as usize;
 
-                print!(
-                    "{:}",
-                    levels[levels.len() * pixel.value / (pixel.scale.end - pixel.scale.start)]
-                )
+                if let Some(pixel) = &glyph.dots.get(index) {
+                    let levels = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'];
+                    let index = levels.len() * pixel.value / (pixel.scale.end - pixel.scale.start);
+
+                    if let Some(level) = levels.get(index) {
+                        print!("{:}", level);
+                    } else {
+                        eprintln!("Encountered a pixel, which brightness level is of bounds.")
+                    }
+                } else {
+                    eprintln!("Encountered a pixel located out of bounds.")
+                }
             }
 
-            if row != glyph.height {
-                println!();
-            }
+            if row != glyph.height { println!(); }
         }
     }
 }
